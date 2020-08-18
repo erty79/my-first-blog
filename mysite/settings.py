03792334ccaf8a4cbfcebc37b92d9d5144ae197e
erty79 <69153583+erty79@users.noreply.github.com>
2020-08-18 16:21:12 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+fn$(&8zs0npsbp%iq+&fi+j-@gf3dfw9!e@9me%)rwqbs0%c&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
